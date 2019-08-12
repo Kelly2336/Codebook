7aab7e5d66412499e19f4ff69508988f45cda4f5
Hyperbola <hyperbola.cs07@gmail.com>
2019-08-12 12:53:37 +0800

    Some codebook

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -1,47 +1,46 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define INF 2100000000
-#define maxn 1000
-typedef pair<int,int> pii;
-void Dijkstra(int n, vector<pii> G[], int d[], int s, int t = -1)
-{
-    for(int i=1; i<=n; i++)
-        d[i] = INF;
-    d[s] = 0;
-    priority_queue<pii> pq;
-    pq.push({0,s});
-    while(!pq.empty())
-    {
-        int u = pq.top().second;
-        pq.pop();
-        if(u==t)return;
-        for(pii e:G[u])
-        {
-            int v = e.first;
-            int w = e.second;
-            if( d[v] > d[u]+w )
-            {
-                d[v] = d[u]+w;
-                pq.push({d[v],v});
+// Find the mininum path from src to dest.
+int dijkstra(int src, int dest, const vector<vector<pii>>& edge) {
+    vector<bool> vis(edge.size(), 0);
+    // A comparator may be required.
+    priority_queue<pii, vector<pii>, greater<pii>> q;
+    q.push(make_pair(0, src));
+    while (!q.empty()) {
+        int v = q.top().second;
+        int d = q.top().first;
+        q.pop();
+        if (v == dest) return d;
+        if (vis[v]) continue;
+        vis[v] = true;
+        for (auto& e : edge[v]) {
+            if (!vis[e.second]) {
+                // Fit the comparator
+                q.push(make_pair(d + e.first, e.second));
             }
         }
     }
+    throw "Never happens.";
 }
-int main()
-{
-    vector<pii> G[maxn];
-    int d[maxn];
-    int n,m;
-    cin >> n >> m;
-    for(int i=1;i<=m;i++)
-    {
-        int a,b,w;
-        cin >> a >> b >> w;
-        G[a].push_back({b,w});
+
+// Find the minuimum path from src to all the other vertices.
+vector<int> dijkstra(int src, const vector<vector<pii>>& edge) {
+    vector<int> mindist(edge.size(), -1);
+    int vis = 0;
+    // A comparator may be required.
+    priority_queue<pii, vector<pii>, greater<pii>> q;
+    q.push(make_pair(0, src));
+    while (vis < edge.size()) {
+        int v = q.top().second;
+        int d = q.top().first;
+        q.pop();
+        if (mindist[v] != -1) continue;
+        mindist[v] = d;
+        vis++;
+        for (auto& e : edge[v]) {
+            if (mindist[e.second] == -1) {
+                // Fit the comparator
+                q.push(make_pair(d + e.first, e.second));
+            }
+        }
     }
-    int s,t;
-    cin >> s >> t;
-    Dijkstra(n,G,d,s,t);
-    printf("%d\n",d[t]);
-    return 0;
+    return mindist;
 }
