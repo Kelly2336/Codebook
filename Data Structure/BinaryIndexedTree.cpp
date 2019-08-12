60ac86aa22c345f8d8c908841d9274ff10cdb242
Hyperbola <hyperbola.cs07@gmail.com>
2019-08-12 14:47:20 +0800

    Fix BIT

--- Data Structure/BinaryIndexedTree.cpp
+++ Data Structure/BinaryIndexedTree.cpp
@@ -6,18 +6,17 @@ struct BIT {
     int N;
     vector<ll> bitarray;
     BIT(int n) : N(n) { bitarray.assign(N + 1, 0); }
-    // update single value
+    // Increase element at index by val
     void add(int index, ll val) {
         index++;
         while (index <= N) bitarray[index] += val, index += index & -index;
     }
-    // query range [0, index]
     ll query(int index) {
         index++;
         ll ret = 0;
         while (index > 0) ret += bitarray[index], index -= index & -index;
         return ret;
     }
-    // query range [left_inc, right_inc]
+    // Query sum in [left_inc, right_inc]
     ll query(int left_inc, int right_inc) { return query(right_inc) - query(left_inc - 1); }
 };
